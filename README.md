4fee868fb84c79fa08c5f66d625286cd3ed062bc
Fandrian Rhamadiansyah <fandrian.rhamadiansyah@gmail.com>
2022-05-25 10:28:33 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# iPokeDex
+
+## About
+iPokeDex is a simple iOS app to search pokemon powered with [PokeAPI.](https://pokeapi.co/)
+
---
+++ iPokeDex/iPokeDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPokeDexApp.swift
+//  iPokeDex
+//
+//  Created by Fandrian Rhamadiansyah on 25/05/22.
+//
+
+import SwiftUI
+
+@main
+struct iPokeDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
